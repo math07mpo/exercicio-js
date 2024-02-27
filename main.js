feaba4258ef0ccfb302371a6c2437f6a547b8190
math07mpo <127318836+math07mpo@users.noreply.github.com>
2024-02-27 08:42:17 -0300

    sxsxs

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function mudarCorTexto(){
 
-document.querySelector("#p1").style.color="green"
+document.querySelector("#p1").style.color= "black"
 
+
+}
+function mudarCorFundo(){
+  document.querySelector("#p1").style.color= "white"
 
 }
